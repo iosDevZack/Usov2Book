99ee4ae43a78cde26e50dc2c55a71e03bc0b7440
iosDevZack <86024420+iosDevZack@users.noreply.github.com>
2022-01-25 23:15:12 +0300

    1 day

---
+++ day 1/UsovExample1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UsovExample1
+//
+//  Created by Женя  on 14.01.22.
+// page 42
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
