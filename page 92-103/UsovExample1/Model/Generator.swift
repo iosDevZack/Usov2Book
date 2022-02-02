8f357d627ae080f3b52494985b0d2209b47db8da
iosDevZack <86024420+iosDevZack@users.noreply.github.com>
2022-02-02 19:11:40 +0300

    Page 92-103

---
+++ page 92-103/UsovExample1/Model/Generator.swift
@@ -0,0 +1,28 @@
+//
+//  Generator.swift
+//  UsovExample1
+//
+//  Created by Женя  on 2.02.22.
+//
+
+import Foundation
+
+protocol GeneratorProtocol {
+    func getRandomValue() -> Int
+}
+
+class NumberGenerator: GeneratorProtocol {
+    private let startRangeValue: Int
+    private let endRangeValue: Int
+    init?(startValue: Int, endValue: Int) {
+        guard startValue <= endValue else {
+            return nil
+        }
+        startRangeValue = startValue
+        endRangeValue = endValue
+    }
+    func getRandomValue() -> Int {
+        (startRangeValue...endRangeValue).randomElement()!
+    }
+    
+}
