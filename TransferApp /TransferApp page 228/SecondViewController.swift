0e5961663631fc7c86c89789b40d342f32de7fed
iosDevZack <86024420+iosDevZack@users.noreply.github.com>
2022-03-01 19:36:33 +0300

    Page 249

---
+++ TransferApp /TransferApp page 228/SecondViewController.swift
@@ -0,0 +1,52 @@
+//
+//  SecondViewController.swift
+//  TransferApp page 228
+//
+//  Created by Женя  on 25.02.22.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    @IBOutlet weak var dataTextField: UITextField!
+    
+    var updatingData: String = ""
+    
+    var handleUpdatedDataDelegate: DataUpdateProtocol?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        updateTextFieldData(withText: updatingData)
+        
+    }
+    
+    private func updateTextFieldData(withText text: String) {
+        dataTextField.text = text
+    }
+    
+    @IBAction func saveDataWithProperty(_ sender: Any) {
+        self.navigationController?.viewControllers.forEach({ viewController in
+            (viewController as? ViewController)?.updatedData = dataTextField.text ?? ""
+        })
+    }
+    
+    
+    // Переход от Б к А
+    // Передача данных с помощью делегата
+    @IBAction func saveDataWithDelegate (_ sender: UIButton) {
+        // получаем обновленные данные
+        let updatedData = dataTextField.text ?? ""
+        
+        // вызываем метод делегата
+        handleUpdatedDataDelegate?.onDataUpdate(data: updatedData)
+        
+        // возвращаемся на предыдущий экран
+        navigationController?.popViewController(animated: true)
+    }
+}
