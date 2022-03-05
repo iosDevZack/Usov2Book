71d0ebd586a99b4bed53b8360266232091b778f1
iosDevZack <86024420+iosDevZack@users.noreply.github.com>
2022-03-05 23:50:09 +0300

    Page 257 - 341

---
+++ To-Do Manager/To-Do Manager/TaskEditController.swift
@@ -0,0 +1,136 @@
+//
+//  TaskEditController.swift
+//  To-Do Manager
+//
+//  Created by Женя  on 5.03.22.
+//
+
+import UIKit
+
+class TaskEditController: UITableViewController {
+    
+    @IBOutlet var taskTitle: UITextField!
+    @IBOutlet var taskTypeLabel: UILabel!
+    
+    @IBOutlet var taskStatusSwitch: UISwitch!
+    
+    // параметры задачи
+    var taskText: String = ""
+    var taskType: TaskPriority = .normal
+    var taskStatus: TaskStatus = .planned
+    
+    var doAfterEdit: ((String, TaskPriority, TaskStatus) -> Void)?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // обновление текстового поля с названием задачи
+        taskTitle?.text = taskText
+        
+        // обновление метки в соответствии текущим типом
+        taskTypeLabel?.text = taskTitles[taskType]
+        
+        // обновляем статус задачи
+        if taskStatus == .completed {
+            taskStatusSwitch.isOn = true
+        }
+    }
+    
+//    Прежде, чем сцена будет отображена на экране, необходимо обновить значе- ние в правой текстовой метке. Для этого, в первую очередь, создадим словарь соответствия типа и строкового значения, которое должно быть отображено в правой текстовой метке.
+    
+    // Название типов задач
+    private var taskTitles: [TaskPriority: String] = [.important: "Важная", .normal: "Текущая"]
+    
+    @IBAction func saveTask(_ sender: UIButton) {
+        // получаем актуальные значения
+        let title = taskTitle?.text ?? ""
+        let type = taskType
+        let status: TaskStatus = taskStatusSwitch.isOn ? .completed : .planned
+        
+        // вызываем обработчик
+        doAfterEdit?(title, type, status)
+        
+        // возвращаемся к предыдущему экрану
+        navigationController?.popViewController(animated: true)
+    }
+
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toTaskTypeScreen" {
+
+            // ссылка на контроллер назначения
+            let destination = segue.destination as! TaskTypeController
+
+            // передача выбранного типа
+            destination.selectedType = taskType
+
+            // передача обработчика выбора типа
+            destination.doAfterTypeSelected = { selectedType in
+                self.taskType = selectedType
+
+                // обновляем метку с текущим типом
+                self.taskTypeLabel?.text = self.taskTitles[self.taskType]
+            }
+        }
+    }
+
+
+}
---
+++ To-Do Manager/To-Do Manager/View/Cells/TaskTypeCell.swift
@@ -0,0 +1,26 @@
+//
+//  TaskTypeCell.swift
+//  To-Do Manager
+//
+//  Created by Женя  on 5.03.22.
+//
+
+import UIKit
+
+class TaskTypeCell: UITableViewCell {
+    
+    @IBOutlet var typeTitle: UILabel!
+    @IBOutlet var typeDescription: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
