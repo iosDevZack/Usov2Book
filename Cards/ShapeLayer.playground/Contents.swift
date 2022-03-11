13cbb8aea8f1910f5cee9240fb9bd687af4f41ab
iosDevZack <86024420+iosDevZack@users.noreply.github.com>
2022-03-12 00:25:47 +0300

    Page 401 - 447

--- Cards/ShapeLayer.playground/Contents.swift
+++ Cards/ShapeLayer.playground/Contents.swift
@@ -11,6 +11,15 @@ class MyViewController : UIViewController {
         
         // круг
         view.layer.addSublayer(CircleShape(size: CGSize(width: 200, height: 150), fillColor: UIColor.gray.cgColor))
+        
+        // игральная карточка рубашкой вверх
+        let firstCardView = CardView<CircleShape>(frame: CGRect(x: 0, y: 0, width: 120, height: 150), color: .red)
+        self.view.addSubview(firstCardView)
+        
+        // игральная карточка лицевой стороной вверх
+        let secondCardView = CardView<CircleShape>(frame: CGRect(x: 200, y: 0, width: 120, height: 150), color: .red)
+        self.view.addSubview(secondCardView)
+        secondCardView.isFlipped = true
     }
 }
 // Present the view controller in the Live View window
@@ -151,6 +160,170 @@ class BackSideCircle: CAShapeLayer, ShapeLayerProtocol {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+}
+
+class BlackSideLine: CAShapeLayer, ShapeLayerProtocol {
+    required init(size: CGSize, fillColor: CGColor) {
+        super.init()
+        
+        let path = UIBezierPath()
+        
+        // рисуем 15 линий
+        for _ in 1...15 {
+            
+            // координаты начала очередной линии
+            let randomXStart = Int.random(in: 0...Int(size.width))
+            let randomYStart = Int.random(in: 0...Int(size.height))
+            
+            // координаты конца очередной линии
+            let randomXEnd = Int.random(in: 0...Int(size.width))
+            let randomYEnd = Int.random(in: 0...Int(size.height))
+            
+            // смещаем указатель к началу линии
+            path.move(to: CGPoint(x: randomXStart, y: randomYStart))
+            
+            // рисуем линию
+            path.addLine(to: CGPoint(x: randomXEnd, y: randomYEnd))
+        }
+        
+        // инициализируем созданный путь
+        self.path = path.cgPath
+        
+        // изменяем стиль линий
+        self.strokeColor = fillColor
+        self.lineWidth = 3
+        self.lineCap = .round
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+protocol FlippableView: UIView {
+    var isFlipped: Bool {get set}
+    var flipCompletionHandler: ((FlippableView) -> Void)? {get set}
+    func flip()
+}
+
+class CardView<ShapeType: ShapeLayerProtocol>: UIView, FlippableView {
+    
+    var isFlipped: Bool = false {
+        didSet {
+            //сообщает о том, что вью необходимо перерисовать полностью.
+            self.setNeedsDisplay()
+        }
+    }
+    var flipCompletionHandler: ((FlippableView) -> Void)?
+    
+    func flip() {}
+    
+    // цвет фигуры
+    var color: UIColor!
+    
+    // радиус закругления
+    var cornerRadius = 20
+    
+    init(frame: CGRect, color: UIColor) {
+        super.init(frame: frame)
+        self.color = color
+        
+
+        setupBorders()
+    }
+    
+    override func draw(_ rect: CGRect) {
+        //Удаляем добавленные ранее дочерние представления
+        backSideView.removeFromSuperview()
+        frontSideView.removeFromSuperview()
+        
+        // добавляем новые представления
+        if isFlipped {
+            self.addSubview(backSideView)
+            self.addSubview(frontSideView)
+        } else {
+            self.addSubview(frontSideView)
+            self.addSubview(backSideView)
+        }
+        
+        
+    }
+    
+    // настройка границ
+    private func setupBorders() {
+        self.clipsToBounds = true
+        self.layer.cornerRadius = CGFloat(cornerRadius)
+        self.layer.borderWidth = 2
+        self.layer.borderColor = UIColor.black.cgColor
+    }
+    
+
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // внутренний отступ представления
+    private let margin: Int = 10
+    
+    // представление с лицевой стороной карты
+    lazy var frontSideView: UIView = self.getFrontSideView()
+    
+    // представление с обратной стороной карты
+    lazy var backSideView: UIView = self.getBackSideView()
+    
+    // возвращает представление для лицевой стороны карточки
+    private func getFrontSideView() -> UIView {
+        let view = UIView(frame: self.bounds)
+        view.backgroundColor = .white
+        
+        let shapeView = UIView(frame: CGRect(x: margin, y: margin, width: Int(self.bounds.width)-margin*2, height: Int(self.bounds.height)-margin*2))
+        view.addSubview(shapeView)
+        
+        // создание слоя с фигурой
+        let shapeLayer = ShapeType(size: shapeView.frame.size, fillColor: color.cgColor)
+        shapeView.layer.addSublayer(shapeLayer)
+        
+        return view
+    }
+    
+    // возвращает вью для обратной стороны карточки
+    private func getBackSideView() -> UIView {
+        let view = UIView(frame: self.bounds)
+        
+        view.backgroundColor = .white
+        
+        //выбор случайного узора для рубашки
+        switch ["circle", "line"].randomElement()! {
+        case "circle":
+            let layer = BackSideCircle(size: self.bounds.size, fillColor: UIColor.black.cgColor)
+            view.layer.addSublayer(layer)
+        case "line":
+            let layer = BlackSideLine(size: self.bounds.size, fillColor: UIColor.black.cgColor)
+            view.layer.addSublayer(layer)
+        default:
+            break
+        }
+        return view
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("touchesBegan Card")
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("touchesMoved Card")
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("touchesEnded Card")
+        
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("touchesCancelled Card")
+    }
+    
 }
 
 
